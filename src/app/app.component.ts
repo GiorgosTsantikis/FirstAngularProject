c832d5b4e1c9f7a5d842d5bb7eea8d3440726fd4
GiorgosTsantikis <gtsantiki@csd.auth.gr>
2024-11-29 18:44:24 +0200

    first angular proj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
 @Component({
+  standalone:true,
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,HomeComponent],
+  template:
+  
+  `
+  <main>
+  <header class="brand-name">
+  <img class="brand-logo" src="/app/logo.svg"/>
+  </header>
+  <section class="content">
+    <app-home/>
+  </section>
+  </main>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
